bcaf6ce777f57f787aafeb5ee835804ca6c5ce09
TheMythologist <leekaixuan2001@gmail.com>
2023-04-02 12:37:45 +0800

    tests: add tests for unsigned salt in locksettings.db

--- tests/test_locksettings.py
+++ tests/test_locksettings.py
@@ -7,17 +7,13 @@ from cracker.locksettings import retrieve_salt
 
 
 def test_locksetting():
-    assert retrieve_salt("sample/locksettings.db") == 1059186646558953472
+    assert retrieve_salt("sample/locksettings/unsigned_locksettings.db") == 1059186646558953472
+    assert retrieve_salt("sample/locksettings/signed_locksettings.db") == 17387557427150598144
 
 
 def test_bad_locksettings():
     with pytest.raises(
         InvalidFileException,
-        match=re.escape('Invalid salt value in database (found "asdf")'),
-    ):
-        retrieve_salt("sample/invalidlocksettings.db")
-    with pytest.raises(
-        InvalidFileException,
         match=re.escape("No salt value in database"),
     ):
-        retrieve_salt("sample/badlocksettings.db")
+        retrieve_salt("sample/locksettings/bad_locksettings.db")
